290adcd75b12047e35861facb4114391175c6613
EC2 Default User <ec2-user@ip-172-31-30-107.ca-central-1.compute.internal>
2018-03-26 14:18:38 +0000

    Linkedin works-ish

--- src/main/java/com/openspection/auth/WebSecurityConfig.java
+++ src/main/java/com/openspection/auth/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.openspection.auth;
 
+import com.openspection.auth.service.LinkedinSignInAdapter;
+import com.openspection.auth.service.LinkedinConnectionSignup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,11 +14,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
+
 @Configuration
 @EnableWebSecurity
+//@WebAppConfiguration
+//@ComponentScan(basePackages = { "com.openspection.auth.service" })
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private ConnectionFactoryLocator connectionFactoryLocator;
+
+    @Autowired
+    private UsersConnectionRepository usersConnectionRepository;
+
+    @Autowired
+    private LinkedinConnectionSignup linkedinConnectionSignup;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -49,4 +68,12 @@ public AuthenticationManager authenticationManagerBean()
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
+ @Bean
+    // @Primary
+    public ProviderSignInController providerSignInController() {
+        ((InMemoryUsersConnectionRepository) usersConnectionRepository).setConnectionSignUp(linkedinConnectionSignup);
+	ProviderSignInController controller = new ProviderSignInController(connectionFactoryLocator, usersConnectionRepository, new LinkedinSignInAdapter());
+	controller.setApplicationUrl("http://www.codetally.com:8120");
+	return controller;
+	}
 }
---
+++ src/main/java/com/openspection/auth/service/LinkedinConnectionSignup.java
@@ -0,0 +1,28 @@
+package com.openspection.auth.service;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+
+import com.openspection.auth.repository.UserRepository;
+import com.openspection.auth.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LinkedinConnectionSignup implements ConnectionSignUp {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public String execute(Connection<?> connection) {
+        System.out.println("signup === ");
+        final User user = new User();
+        user.setUsername(connection.getDisplayName());
+        user.setPassword(randomAlphabetic(8));
+        userRepository.save(user);
+        return user.getUsername();
+    }
+
+}
